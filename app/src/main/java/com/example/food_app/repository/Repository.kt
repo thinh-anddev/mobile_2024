3722ac7acf7363dde81169efee5639beede62b95
thinhnt <thinhnt.ntd.vn@gmail.com>
2024-03-26 20:53:07 +0700

    add food

--- app/src/main/java/com/example/food_app/repository/Repository.kt
+++ app/src/main/java/com/example/food_app/repository/Repository.kt
@@ -1,17 +1,21 @@
 package com.example.food_app.repository
 
+import com.example.food_app.R
 import com.example.food_app.model.Food
 
 class Repository {
-    private var listFood: MutableList<Food> = ArrayList()
-    fun listFood(): MutableList<Food> {
+    companion object {
+        private var listFood: MutableList<Food> = ArrayList()
+        fun listFood(): MutableList<Food> {
 
-//        listFood.add(
-//            Food(
-//
-//            )
-//        )
+            listFood.add(
+                Food(
+                    1,"Pizza gà",1231.0,"Pizza","con","Pizza gà có lẽ vô cùng quen thuộc với dân nghiện pizza bởi tính phổ thông và dễ ăn của loại pizza này." +
+                            " Thịt gà là một món ăn bổ dưỡng, giúp gia tăng vị giác của bất cứ món ăn nào mà chúng góp mặt. " +
+                            "Đặc biệt những bạn đam mê gà thì không thể bỏ qua chiếc bá", R.drawable.pizzaga,10)
+            )
 
-        return listFood
+            return listFood
+        }
     }
 }
---
+++ app/src/main/java/com/example/food_app/utils/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.food_app.utils
+
+object Constant {
+    const val DATA_PRODUCT = "DATA_PRODUCT"
+}
